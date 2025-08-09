706e337e7ff6b38409fbbf6f30cf2d85949463bd
Clansty <i@gao4.pw>
2025-08-09 14:42:20 +0800

    点 logo 回主页

--- AquaNet/src/App.svelte
+++ AquaNet/src/App.svelte
@@ -53,7 +53,7 @@
 
 <nav>
   {#if path !== "/legacy" && path !== "/legacy/"}
-    <a class="logo" href={USER.isLoggedIn() ? "/legacy/home" : "/"}>
+    <a class="logo" href="/">
       <img src={logo} alt="">
     </a>
   {/if}
