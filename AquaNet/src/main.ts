55ede4525ae016a3dfa2771de635a9ea05e70d8c
Clansty <i@gao4.pw>
2026-02-23 17:19:28 +0800

    remove frontend

--- AquaNet/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from 'svelte';
-import './app.sass'
-import App from './App.svelte'
-import './assets/fonts/Quicksand.400.css'
-import './assets/fonts/Quicksand.500.css'
-
-const app = mount(App, { target: document.getElementById("app")! });
-
-export default app
